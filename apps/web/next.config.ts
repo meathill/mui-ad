0fb26a2a6bec75192b3c025a26a62d5d4a00a9b7
meathill <meathill@users.noreply.github.com>
2026-04-14 23:57:39 +0800

    修正项目配置，添加数据库迁移脚本

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
   images: {
     unoptimized: true,
   },
+  reactStrictMode: false,
 };
 
 export default nextConfig;
